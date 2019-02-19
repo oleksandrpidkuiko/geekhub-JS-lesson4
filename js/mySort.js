62ed10bc2f695cf0126e2d505a61abe5c947ec7a
Oleksandr Pidkuiko <o.pidkuiko111@gmail.com>
2019-02-19 12:29:19 +0200

    Fixed a problem with mySort. Now mySort identicaly work like Array.prototype.sort

--- js/mySort.js
+++ js/mySort.js
@@ -2,8 +2,25 @@
 
 Array.prototype.mySort = function(fn) {
 
-    for (let i = 0; i < this.length; i++) {
-        this[i] = fn(this[i-1], this[i]);
+    if (typeof fn === 'undefined') {
+        for (let i = 0; i < this.length; i++) {
+            for (let j = 0; j < this.length-1-i; j++) {
+                if (String.prototype.charCodeAt.call(this[j+1],0) < String.prototype.charCodeAt.call(this[j],0)) {
+                let temp = this[j+1];
+
+                this[j+1] = this[j];
+                this[j] = temp;
+                }
+            }
+        }
+
+        return this;
+
+    } else {
+
+        for (let i = 0; i < this.length; i++) {
+            this[i] = fn(this[i-1], this[i]);
+        }
     }
 
     return this;
